d94b74decea1548241bb7a8a8e5b70cab24ce93b
Manuel <mninahuanca4@gmail.com>
2021-06-06 20:00:08 -0400

    otros tipos de datos

--- 01-HELLO-PROPS/Hello.js
+++ 01-HELLO-PROPS/Hello.js
@@ -1,5 +1,12 @@
 class Hello extends React.Component {
+
     render() {
-        return <h1>Hi {this.props.to} from {this.props.from}</h1>
+        let bangs = "!".repeat(this.props.bangs);
+        return (
+            <div>
+                <p><h1>Hi {this.props.to} from {this.props.from} {bangs}</h1></p>
+                <img src={this.props.img} alt="" />
+            </div>
+        )
     }
 }
--- 01-HELLO-PROPS/index.js
+++ 01-HELLO-PROPS/index.js
@@ -2,8 +2,13 @@ class App extends React.Component {
     render() {
         return (
             <div>
-                <Hello to="Ringo" from="Paul"/>
-                <Hello to="Cher" from="Sonny"/>
+                <Hello
+                    to="Ringo"
+                    from="Paul"
+                    bangs={4}
+                    img="https://images.unsplash.com/photo-1622987437805-5c6f7c2609d7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1489&q=80"
+                />
+
             </div>
         )
     }
